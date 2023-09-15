7f04deec13df7e1f9996456605c4ec55648f2357
dannabelleli <belleli.danna@gmail.com>
2023-09-14 20:08:29 -0700

    added functionality to search a spesific show. Also added some styling to the page.

--- client/pages/Home.jsx
+++ client/pages/Home.jsx
@@ -1,10 +1,14 @@
 //homepage which will render popular tv shows from API, where users can choose to leave reviews. Also contains searchbar for fetching specific results.
-//homepage which will render popular tv shows from API, where users can choose to leave reviews. Also contains searchbar for fetching specific results.
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function Home() {
-    const [shows, setShows] = useState([]);
+    let [shows, setShows] = useState([]);
+    let [searchInput, setSearchInput] = useState('');
+
+    const handleSearchInput = (e) => {
+        setSearchInput(e.target.value)
+    }
 
     useEffect(() => {
         axios.get('https://api.themoviedb.org/3/discover/movie?api_key=977f0b8bb93b6b77c36f6cbeb9c655a3')
@@ -16,16 +20,37 @@ export default function Home() {
             });
     }, []);
 
+    const fetchDataFromTMDb = async () => {
+        try {
+            const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=977f0b8bb93b6b77c36f6cbeb9c655a3&query=${searchInput}`)
+            console.log(response.data);
+            console.log(searchInput);
+            setSearchInput('');
+            setShows(response.data.results);
+        } catch (err) {
+            console.error('Error fetching data', err);
+        }
+    };
+
     return (
         <>
-            <input type="text" name="searchbar" id="searchbar" placeholder="Search for a show" />
-            <ul>
+            <div className="flex w-96 justify-between p-5">
+                <input className='' type="text" name="searchbar" id="searchbar" value={searchInput} onChange={handleSearchInput} placeholder="Search for a show" />
+                <button id="btnSearch" onClick={fetchDataFromTMDb} className='bg-cyan-600 hover:bg-cyan-800 w-16 h-10 text-white rounded-md'>Search</button>
+            </div>
+            <ul className='p-5'>
                 {shows.map(show => (
-                    <li key={show.id}>{show.title}</li>
+                    <div key={show.id} className="flex mb-4 w-96 justify-between">
+                        <li>{show.title}</li>
+                        <button className=" cursor-pointer bg-cyan-600 hover:bg-cyan-800 w-8 h-8 rounded-full text-white	
+
+">+</button>
+                    </div>
                 ))}
             </ul>
         </>
     );
 }
+
 
 
